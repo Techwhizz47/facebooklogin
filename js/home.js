af87e2ff336548acf871420e6919ed82a81ad10e
Techwhizz47 <allanndungu47@gmail.com>
2024-05-29 21:26:08 +0300

    signinout

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-    
+document.getElementById('signout').onclick = function(){
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        window.location.href = "/login.html"
+      }).catch((error) => {
+        // An error happened.
+      });
+}
--- js/login.js
+++ js/login.js
@@ -1,3 +1,20 @@
 function next(){
     window.location.href = "/signup.html";
 }
+
+document.getElementById('login').onclick = function(){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('pass').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+     }).then(()=>{window.location.href = "/home.html"})
+        .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+
+}
